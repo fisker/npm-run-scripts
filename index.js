dae2546e0e9b038cc84bd4d6f07132fd316cefec
fisker <lionkay@gmail.com>
2018-05-04 10:08:56 +0800

    rewrite

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-var dir = process.cwd()
+var CWD = process.cwd()
 var spawn = require('cross-spawn')
+var scriptName = process.argv[2]
+var scripts = getScripts()
 
-if (process.argv[2]) {
-  runNpmScript(process.argv[2])
+if (scriptName) {
+  runNpmScript(scriptName)
 } else {
+  promptScripts(scripts).then(runNpmScript)
+}
+
+function promptScripts(scripts) {
   var inquirer = require('inquirer')
   var inquirerAutocompletePrompt = require('inquirer-autocomplete-prompt')
   var fuzzy = require('fuzzy')
-  var join = require('path').join
   var Promise = global.Promise || require('es6-promise').Promise
 
   inquirer.registerPrompt('autocomplete', inquirerAutocompletePrompt)
 
-  var scripts = require(join(dir, 'package.json')).scripts
-
-  if (!scripts) {
-    process.stderr.write('ERROR: No scripts found!')
-    process.exit(1)
-  }
-
-  var keys = Object.keys(scripts)
-
-  inquirer
+  return inquirer
     .prompt({
       type: 'autocomplete',
       name: 'script',
-      message: 'Choice a script to run?',
+      message: 'Choice a script to run',
       source: function(answers, input) {
-        var result = fuzzy.filter(input || '', keys)
+        var result = fuzzy.filter(input || '', scripts)
         result = result.map(function(el) {
           return el.original
         })
@@ -36,20 +32,49 @@ if (process.argv[2]) {
       pageSize: 10
     })
     .then(function(answers) {
-      var script = answers.script
-
-      if (!scripts[script]) {
-        process.stderr.write('ERROR: missing script: ' + script + '')
-        process.exit(1)
-      }
-
-      runNpmScript(script)
+      return answers.script
     })
 }
 
-function runNpmScript(script) {
-  spawn('npm', ['run', script], {
+function showError(msg) {
+  process.stderr.write('ERROR: ' + msg)
+  process.exit(1)
+}
+
+function getScripts() {
+  var join = require('path').join
+  var pkg
+  var scriptKeys
+
+  try {
+    pkg = require(join(CWD, 'package.json'))
+  } catch (err) {
+    showError('No package.json found!')
+  }
+
+  var scripts = pkg.scripts
+
+  if (!scripts || typeof scripts !== 'object') {
+    showError('No scripts found!')
+  }
+
+  var scriptKeys = Object.keys(scripts)
+
+  if (!scriptKeys.length) {
+    showError('No scripts found!')
+  }
+
+  return scriptKeys
+}
+
+function runNpmScript(scriptName) {
+  if (!scripts.includes(scriptName)) {
+    process.stderr.write('ERROR: missing script: ' + scriptName + '')
+    process.exit(1)
+  }
+
+  spawn('npm', ['run', scriptName], {
     stdio: 'inherit',
-    cwd: dir
+    cwd: CWD
   })
 }
