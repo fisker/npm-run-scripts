014da8b94f89153eb7ce4dfd53bb7f185a7e0108
fisker <lionkay@gmail.com>
2019-11-24 01:59:42 +0800

    chore: minor tweak

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import filesize from 'rollup-plugin-filesize'
 import json from 'rollup-plugin-json'
 import commonjs from 'rollup-plugin-commonjs'
 import {dependencies} from './package.json'
@@ -6,10 +5,10 @@ import {dependencies} from './package.json'
 export default {
   input: './src/cli.js',
   output: {
-    file: './bin/cli',
+    file: './dist/cli',
     format: 'cjs',
     banner: '#!/usr/bin/env node',
   },
-  plugins: [commonjs(), json(), filesize()],
+  plugins: [commonjs(), json()],
   externals: [...Object.keys(dependencies), './package.json'],
 }
